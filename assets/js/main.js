2fd7c8bda9ca5a1ae8fb8750b82a8f8a09a20be9
gshen-uh <guoyang.shen@outlook.com>
2024-05-06 00:35:23 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -104,7 +104,7 @@
 					overlayOpacity: 0.85,
 					popupCloserText: '',
 					popupLoaderText: '',
-					selector: '.work-item a.image',
+					selector: '.work-item a.image:not(.external)', // Exclude links with class 'external'
 					usePopupCaption: true,
 					usePopupDefaultStyling: false,
 					usePopupEasyClose: false,
